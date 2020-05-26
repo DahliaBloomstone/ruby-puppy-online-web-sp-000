d97984457bfce1e0a23d9988fb7215a5129579a7
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-26 00:15:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,26 @@
 class Dog
-  @@all = []
+  @@all = []       #has a class variable, @@all, the points to an array
 
-  def initialize(name)
+  def initialize(name) #initializes with one argument - a name
     @name = name
     self.save
   end
 
-  def self.all
+  def self.all  #is a class method returns all dog instances
     @@all
   end
 
-  def self.clear_all
+  def self.clear_all #is a class method that empties the @@all array of all existing dogs
     @@all = []
   end
 
-  def self.print_all
+  def self.print_all #is a class method that puts out the name of each dog to the terminal
     @@all.each do |dog|
       puts dog.name
     end
 end
 
-  def save
+  def save #adds this dog instance to the @@all array when called, gets called inside initialize when a new Dog is created
     @@all << self
   end
 
